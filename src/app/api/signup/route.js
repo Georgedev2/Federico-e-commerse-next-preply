95f16196c291aacfacd2966fc116ba05922fc35d
george <v-gnwobodo@digitalocean.com>
2024-09-12 21:40:41 +0100

    worked on the sign up page

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -1,7 +1,54 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { db } from '../../dbConfig/config';
+import bcrypt from 'bcrypt';
 
-export const POST= async(req, res)=>{
-    const body= await req.json();
-    
-    return NextResponse.json(body);
-}
+export const POST = async (req, res) => {
+  try {
+    const body = await req.json();
+    const { email, firstName, lastName, password } = body;
+    console.log('vvv', { email, firstName, lastName, password });
+    const matchingUser = await db.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    // 12345gdhdh
+
+    if (matchingUser == null) {
+      const hash = await bcrypt.hash(password, 10);
+
+      const newUser = await db.user.create({
+        data: {
+          email: email,
+          name: `${firstName} ${lastName}`,
+          password: hash,
+        },
+      });
+
+      return NextResponse.json(
+        {
+          message: 'successfully created',
+          user: newUser,
+        },
+        {
+          status: 201,
+        }
+      );
+    }
+    return NextResponse.json({
+      message: 'User already exist',
+    });
+  } catch (error) {
+
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      { status: 500 }
+    );
+  }
+  // {
+  //     stack, message
+  // }
+  //   return NextResponse.json(body);
+};
--- src/app/dbConfig/config.js
+++ src/app/dbConfig/config.js
@@ -3,3 +3,6 @@ import { PrismaClient } from '@prisma/client'
 export const db = new PrismaClient()
 export default db
 // use `prisma` in your application to read and write data in your DB
+
+
+// BBMS server - db client- 
